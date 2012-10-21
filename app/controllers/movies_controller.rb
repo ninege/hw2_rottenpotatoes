3d17d829b462adbd258f3ce53c6e2352fb70a126
Yi Wang <ninege@gmail.com>
2012-10-22 00:42:04 +1100

    hw2-part2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,37 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @all_ratings = Movie.ratings
+ 
+    if params[:sort_key]
+      session[:sort_key] = params[:sort_key]
+      
+    elsif session[:sort_key]
+      flag = true
+      params[:sort_key] = session[:sort_key]
+      
+    end
+    
+    qstring =  Movie.order(params[:sort_key])  
+  
+    if params[:commit] == 'Refresh'
+      session[:ratings] = params[:ratings]
+      
+    elsif session[:ratings] != params[:ratings]
+      flag = true
+      params[:ratings] = session[:ratings]
+      
+    end
+    
+    @ratings, @sort_key = session[:ratings], session[:sort_key]
+    
+    if (flag)
+      redirect_to movies_path(:sort_key => @sort_key,:ratings => @ratings)
+    else
+      puts "ashwani" + (@ratings.nil? ? false : @ratings.has_key?('R')).to_s
+      @movies = @ratings.nil? ? qstring.all : qstring.find_all_by_rating(params[:ratings].keys)
+    end
+    
   end
 
   def new
